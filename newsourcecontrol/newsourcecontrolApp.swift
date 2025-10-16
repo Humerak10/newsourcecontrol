7b69b5092696ddc0fb68f22db7e7cfcccf2d5195
Humera <khanhumerashabin272@gmail.com>
2025-10-16 18:34:00 +0530

    Initial Commit

---
+++ newsourcecontrol/newsourcecontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsourcecontrolApp.swift
+//  newsourcecontrol
+//
+//  Created by Humera Khan on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct newsourcecontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
